2c95af0dfc41b4d8c4ee7ae47faee17ad1b532b0
tingfeng1002 <wxiao1002@yeah.net>
2022-05-05 10:09:16 +0800

    docs(设计模式): ok

---
+++ Python/HelloWord.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+print("Hello, World!")
+
+# 严格的缩进
+if True:
+    print("True")
+else:
+    print("False")
+
+# 多长语句 使用反斜杠 \ 实现
+total = time_one + \
+    time_two + \
+    time_three
+# 在  [] {} () 中 不需反斜杠 \
+total = {"item_one", "item_two",
+         "item_three"}
+
+# Number 类型
+'''
+int 整数 长整形
+bool 布尔
+float 浮点型
+complex 复数
+'''
+a = 1.2
+b = 1+2j
+# 字符串
+'''
+
+'''
