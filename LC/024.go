2c7409e462a8d05f65f9a74f5aa899672ae9a6d5
tingfeng1002 <wxiao1002@yeah.net>
2022-06-02 10:33:12 +0800

    docs(LC): 两数相加

---
+++ LC/024.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+// 反转单链表  反转只需要将curr 的上一个节点编程curr.next
+func main() {
+	head := &ListNode{
+		data: 1, next: &ListNode{
+			data: 2, next: nil,
+		},
+	}
+
+	fmt.Println(reverseList(head))
+}
+
+// 迭代法
+func reverseList(head *ListNode) *ListNode {
+	var curr, prev *ListNode = head, nil
+	for curr != nil {
+		next := curr.next
+		curr.next = prev
+		prev = curr
+		curr = next
+	}
+	return prev
+}
