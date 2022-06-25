e96dda6e868483fd3c5ce6495906fc541b3aa473
tingfeng1002 <wxiao1002@yeah.net>
2022-06-25 22:44:46 +0800

    docs(数据结构): 二分查找&& hash table

--- Go/datastruct/binary_search.go
+++ Go/datastruct/binary_search.go
@@ -33,3 +33,40 @@ func searh2(array []int, low, high, target int) int {
 		return mid
 	}
 }
+
+// 查找第一个等于目标值的元素
+
+func search3(array []int, target int) int {
+	low, high := 0, len(array)
+	for low <= high {
+		mid := low + (high-low)>>1
+		if array[mid] > target {
+			high = mid - 1
+		} else if array[mid] < high {
+			low = mid + 1
+		} else {
+			if mid == 0 || array[mid-1] != target {
+				return mid
+			}
+			high = mid - 1
+		}
+	}
+	return -1
+}
+
+// 第一个大于等于
+func search4(array []int, target int) int {
+	low, high := 0, len(array)
+	for low <= high {
+		mid := low + (high-low)>>1
+		if array[mid] >= target {
+			if mid == 0 || array[mid-1] < target {
+				return mid
+			}
+			high = mid - 1
+		} else {
+			low = mid + 1
+		}
+	}
+	return -1
+}
