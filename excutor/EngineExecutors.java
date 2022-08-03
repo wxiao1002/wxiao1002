646f08c2c8def2e84a0cfc00cefb639696a8a7c1
王潇 <wxiao1002@yeah.net>
2022-08-03 19:57:51 +0800

    Add files via upload
    executor demo

---
+++ excutor/EngineExecutors.java
@@ -0,0 +1,27 @@
+package com.iwhalecloud.bassc.lcdp.engine.excutor;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ForkJoinPool;
+
+/**
+ * @author wang xiao
+ * @date 2022/8/3
+ */
+public class EngineExecutors {
+
+    /**
+     * 创建 线程 工厂
+     * @param parallelism 目标并发级别
+     * @param namePrefix 用于定义线程名称
+     * @return 线程工厂
+     */
+    public static ExecutorService newWorkStealingPool(int parallelism, String namePrefix) {
+        return new ForkJoinPool(parallelism,
+                new EngineForkJoinWorkerThreadFactory(namePrefix),
+                null, true);
+    }
+
+    public static ExecutorService newWorkStealingPool(int parallelism, Class<?> clazz) {
+        return newWorkStealingPool(parallelism, clazz.getSimpleName());
+    }
+}
